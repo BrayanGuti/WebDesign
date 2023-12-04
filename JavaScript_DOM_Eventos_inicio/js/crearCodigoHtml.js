75799e6f6a13cb42a4adde489236e2de9abf8b06
Brayan <Brag.866@gmail.com>
2023-12-03 22:14:08 -0500

    comenzando con El proyecto de Ajedrez

--- JavaScript_DOM_Eventos_inicio/js/crearCodigoHtml.js
+++ JavaScript_DOM_Eventos_inicio/js/crearCodigoHtml.js
@@ -39,17 +39,60 @@ navegacion.appendChild(nuevoEnlace)
 // })
 
 
-const bitEnviar = document.querySelector(".boton--primario")
-bitEnviar.addEventListener("click", function(evento){
-    console.log(evento);
-    evento.preventDefault();  //evita que se recargue la pagina
-    console.log("enviando formulatio");
-})
+// const bitEnviar = document.querySelector(".boton--primario")
+// bitEnviar.addEventListener("click", function(evento){
+//     console.log(evento);
+//     evento.preventDefault();  //evita que se recargue la pagina
+//     console.log("enviando formulatio");
+// })
 
-const nombre = document.querySelector("#nombre")
+
+const nombre = document.querySelector("#nombre");
+const email = document.querySelector("#email");
+const mensaje = document.querySelector("#mensaje");
+const formulario = document.querySelector(".formulario");
+
+const user = {
+    nombre: "",
+    email: "",
+    mensaje: ""
+};
+
 nombre.addEventListener("input", function(evento){
-    console.log("enviando formulario");
-})
+    user[evento.target.id] = evento.target.value;
+    console.log(user);
+});
+
+email.addEventListener("input", function(evento){
+    user[evento.target.id] = evento.target.value;
+    console.log(user);
+});
+
+mensaje.addEventListener("input", function(evento){
+    user[evento.target.id] = evento.target.value;
+    console.log(user);
+});
+
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault();
+    const decision = document.createElement("p");
+
+    if(user.nombre !== "" && user.email !== "" && user.mensaje !== ""){
+        decision.textContent = "Enviado correctamente";
+        decision.classList.add("correcto");
+        formulario.appendChild(decision);
+        setTimeout(function(){
+            decision.remove();
+        }, 3000);
+    } else {
+        decision.textContent = "Todos los campos son obligatorios";
+        decision.classList.add("error");
+        formulario.appendChild(decision);
+        setTimeout(function(){
+            decision.remove();
+        }, 3000);
+    }
+});
 
 
 
